80557b2ebc0c593b70ba77e12f3aef9562166770
Milton Chandradas <milton.chandradas@live.com>
2020-06-24 20:07:25 -0400

    shorter syntax

--- srv/tech-service.js
+++ srv/tech-service.js
@@ -1,9 +1,9 @@
-module.exports = cds.service(srv => {
-    srv.before("*", req => console.debug(">>>", req.method, req.target.name));
+module.exports = srv => {
+    srv.before("*", req => console.log(">>>>", req.method, req.target.name));
 
     srv.on("READ", "Products", async (req, next) => {
         const products = await next();
         return products.filter(product => product.status === "active");
     });
-});
+}
 
